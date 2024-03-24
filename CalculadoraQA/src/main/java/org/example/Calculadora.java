b097586bfc4bb5adefc5bcde27f7b9c59b37cd40
Hillary <129438048+HillaryFerreira@users.noreply.github.com>
2024-03-23 23:30:11 -0300

    Update Calculadora.java

--- CalculadoraQA/src/main/java/org/example/Calculadora.java
+++ CalculadoraQA/src/main/java/org/example/Calculadora.java
@@ -7,22 +7,43 @@ package org.example;
 
 public class Calculadora {
     
-    //Soma dois números inteiros
-    public double somar(int num1, int num2) {
+    /**
+     *
+     * @param num1 primeiro numero double que sera usado na soma
+     * @param num2 segundo numero double que sera usado na soma
+     * @return resultado de n1 mais n2
+     */
+
+    public double somar(double num1, double num2) {
         return num1 + num2;
     }
 
-    //Subtrai dois números inteiros
-    public double subtrair(int num1, int num2) {
+    /**
+     *
+     * @param n1 primeiro numero double que sera usado na subtracao
+     * @param n2 segundo numero double que sera usado na subtracao
+     * @return resultado de n1 menos n2
+     */
+    public double subtrair(double num1, double num2) {
         return num1 - num2;
     }
     
-    //Multiplica dois números (Doubles)
+     /**
+     *
+     * @param n1 primeiro numero inteiro que sera usado na multiplicacao
+     * @param n2 segundo numero inteiro que sera usado na multiplicacao
+     * @return resultado de n1 multiplicado por n2
+     */
     public double multiplicar(double num1, double num2) {
         return num1 * num2;
     }
     
-    //Divide dois números (Doubles)
+    /**
+     *
+     * @param n1 primeiro numero inteiro que sera usado na divisao
+     * @param n2 segundo numero inteiro que sera usado na divisao
+     * @return resultado de n1 dividido por n2
+     */
     public double dividir(double num1, double num2) {
         return num1 / num2;
     }
