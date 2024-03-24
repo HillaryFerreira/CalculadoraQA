5b2eb4bfc3ee1ca94d92232f52c27c31b89f680d
Hillary <129438048+HillaryFerreira@users.noreply.github.com>
2024-03-24 00:22:42 -0300

    Delete CalculadoraQA directory

--- CalculadoraQA/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Calculadora"},{"p":"org.example","l":"CalculadoraTeste"}];updateSearchResults();
