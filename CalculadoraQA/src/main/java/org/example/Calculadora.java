98bff1623d197de25bf8b31fefabf5bf60cc7c31
Hillary <129438048+HillaryFerreira@users.noreply.github.com>
2024-03-23 23:23:00 -0300

    Add files via upload

---
+++ CalculadoraQA/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,33 @@
+package org.example;
+
+/**
+ * 
+ * @author Hillary
+ */
+
+public class Calculadora {
+    
+    //Soma dois números inteiros
+    public double somar(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    //Subtrai dois números inteiros
+    public double subtrair(int num1, int num2) {
+        return num1 - num2;
+    }
+    
+    //Multiplica dois números (Doubles)
+    public double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+    
+    //Divide dois números (Doubles)
+    public double dividir(double num1, double num2) {
+        return num1 / num2;
+    }
+    
+    
+    
+    
+}
